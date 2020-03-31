1ef44c68e38e2023ba8b713e92a272cd3cbd0d3f
Artur <26014458+xout@users.noreply.github.com>
2020-03-31 18:58:31 +0200

    Fix dependencies

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+npm list tsc || npm install tsc
+npm list concurrently || npm install concurrently
 #install/update packages
 npm install
 cd server
--- server/src/loaders/express.ts
+++ server/src/loaders/express.ts
@@ -1,6 +1,6 @@
 import express from 'express'
 import { isProduction } from '../../config'
-import Agenda from 'agenda'
+//import Agenda from 'agenda'
 //const Agendash = require('agendash')
 
 const app: express.Application = express()
